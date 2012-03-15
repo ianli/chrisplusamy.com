033f017e0aae4a0d074c7cf2262040a62fb9160a
Ian Li <ianarli@gmail.com>
2012-03-15 03:52:07 -0400

    Added documentation on how to edit the site. Added default.js

--- README.md
+++ README.md
@@ -3,8 +3,33 @@ Chris and Amy's Wedding Site
 
 This is the web design for Chris and Amy's Wedding Site.
 
-Install
--------
+Editing the Site
+----------------
+
+The relevant files and folders for editing the site are:
+
+```
+_layouts/
+  default.php       # The main layout template
+  
+_pages/             # Contains the pages of the site
+  index.html        # Home page
+  venue.html        # Venue page
+  schedule.html     # Schedule page
+  ...
+  
+css/                # Stylesheets folder
+  default.css       # Site stylesheet
+
+img/                # Images folder
+
+js/                 # Javascripts folder
+  default.js        # Site Javascript file
+```
+
+
+Publishing the Site
+-------------------
 
 1. Download the code.
 2. Put the code in a web-visible directory.
--- _layouts/default.php
+++ _layouts/default.php
@@ -68,58 +68,6 @@
 
 <!-- Javascript -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script>
-  (function ($, undefined) {
-    // Resizes the content's minimum height accordingly, 
-    // so that the footer remains at the bottom.
-    function resizeHeight() {
-      var hHeader = $('header').outerHeight()
-        , hNav = $('nav').outerHeight()
-        , hContent = $('#content').outerHeight()
-        , hFooter = $('footer').outerHeight()
-        , hWindow = $(window).outerHeight()
-        , hTotal = hHeader + hNav + hContent + hFooter;
-
-      if (hTotal < hWindow) {
-        var h = hWindow - hHeader - hNav - hFooter;
-        $('#content').css('min-height', h);
-      }
-    }
-    
-    // Resizes the width of the navigation according to its content links,
-    // so that the links are perfectly centered on the page.
-    function resizeNavigation() {
-      var width = 0;
-      
-      $('nav a').each(function () {
-        width += $(this).outerWidth();
-      });
-      
-      $('nav ul').width(width);
-    }
-    
-    // Highlights the current link in the navigation.
-    function highlightCurrent() {
-      $('nav a').each(function () {
-        var $this = $(this)
-          , href = $this.attr('href')
-          , pathname = window.location.pathname
-          , filename = pathname.substr(pathname.lastIndexOf('/') + 1);
-
-        if ((filename.length == 0 && href == 'index.html')
-            || href == filename) {
-          $this.addClass('selected');
-        }
-      });
-    }
-
-    $(document).ready(function() {
-      resizeHeight();
-      resizeNavigation();
-      highlightCurrent();
-    });
-    $(window).resize(resizeHeight);
-  } (jQuery));
-</script>
+<script src="js/default.js"></script>
 </body>
 </html>
